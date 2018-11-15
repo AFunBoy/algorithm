66a50dc8b8e5d758f451715c312d7a59fdf0224a
wangfan <wangfan@fang.com>
2018-11-15 17:01:27 +0800

    1

---
+++ src/main/java/com/funboy/初级/数组/从排序数组中删除重复项.java
@@ -0,0 +1,27 @@
+package com.funboy.初级.数组;
+
+import org.junit.Test;
+
+/**
+ * @Author: 王帆
+ * @CreateTime: 2018-11-15 16:39
+ * @Description:
+ */
+public class 从排序数组中删除重复项 {
+
+    @Test
+    public void go() {
+
+        assert removeDuplicates(null) == 1;
+    }
+
+    @Test
+    public void go2() {
+
+    }
+
+    public int removeDuplicates(int[] nums) {
+        return 0;
+    }
+}
+
