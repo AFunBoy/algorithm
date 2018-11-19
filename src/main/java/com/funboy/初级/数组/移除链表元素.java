e0199307e50a9dd277e0487d8ff027826d59625f
wangfan <wangfan@fang.com>
2018-11-19 15:27:11 +0800

    1

--- src/main/java/com/funboy/初级/数组/移除链表元素.java
+++ src/main/java/com/funboy/初级/数组/移除链表元素.java
@@ -1,5 +1,7 @@
 package com.funboy.初级.数组;
 
+import org.junit.Test;
+
 /**
  * @Author: 王帆
  * @CreateTime: 2018-11-16 14:51
@@ -10,11 +12,42 @@ package com.funboy.初级.数组;
  * 输出: 1->2->3->4->5
  */
 public class 移除链表元素 {
+    @Test
+    public void go() {
+        ListNode a = new ListNode(1);
+        ListNode b = new ListNode(2);
+        ListNode c = new ListNode(3);
+        ListNode d = new ListNode(1);
+        ListNode e = new ListNode(2);
+        ListNode f = new ListNode(3);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = f;
+        ListNode listNode = removeElements(a, 1);
+     //   System.out.println(listNode.val);
+        while (listNode != null) {
+            System.out.println(listNode.val);
+            listNode = listNode.next;
+        }
+
+    }
+
     public ListNode removeElements(ListNode head, int val) {
-        head.
-        ListNode second = head.next;
-        int val1 = second.val;
-        ListNode third = second.next;
+       ListNode temp = new ListNode(-1);
+       ListNode returnNode = temp;
+       while(null != head){
+           if(val == head.val){
+               temp.next = head.next;
+           }else{
+               temp.next = head;
+               temp = temp.next;
+           }
+           head = head.next;
+       }
+
+       return returnNode.next;
     }
 }
 
@@ -22,6 +55,7 @@ public class 移除链表元素 {
 class ListNode {
     int val;
     ListNode next;
+
     ListNode(int x) {
         val = x;
     }
