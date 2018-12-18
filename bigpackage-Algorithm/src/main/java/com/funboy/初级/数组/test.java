62c0e66cf81ce631e6b449612c2aa16769ef9298
wangfan@fang.com <wangfan@fang.com>
2018-12-18 10:24:46 +0800

    1

---
+++ bigpackage-Algorithm/src/main/java/com/funboy/初级/数组/test.java
@@ -0,0 +1,15 @@
+package com.funboy.初级.数组;
+
+import org.junit.Test;
+
+/**
+ * @Author: 王帆
+ * @CreateTime: 2018-12-18 10:19
+ * @Description:
+ */
+public class test {
+    @Test
+    public void go(){
+        System.out.println(123);
+    }
+}
