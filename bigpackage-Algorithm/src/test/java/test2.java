f16f7de903db1b9ca78afb58ab17e2301e113290
wangfan1 <wangfan1@travelsky.com>
2020-09-14 15:36:57 +0800

    增加org.apache.commons.commons-lang3包

--- bigpackage-Algorithm/src/test/java/test2.java
+++ bigpackage-Algorithm/src/test/java/test2.java
@@ -1,10 +1,12 @@
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.atomic.LongAdder;
-import java.util.function.Supplier;
 
 /**
  * @Author: 王帆
@@ -14,14 +16,22 @@ import java.util.function.Supplier;
 public class test2 {
     @Test
     public void test() {
-        for (int i = 0; i <50; i++) {
-            System.out.println("select a.dianboId,a.createTime,b.endTime,b.createTime from live_vod_user_stat_"+i+" a left join live_vod_info b on a.dianboId = b.videoId order by b.endTime desc ;");
-            LongAdder adder  = new LongAdder();
+        for (int i = 0; i < 50; i++) {
+            System.out.println("select a.dianboId,a.createTime,b.endTime,b.createTime from live_vod_user_stat_" + i + " a left join live_vod_info b on a.dianboId = b.videoId order by b.endTime desc ;");
+            LongAdder adder = new LongAdder();
             //System.out.println("select * from live_vod_user_stat_"+i+" order by createTime desc;");
             ThreadLocal<SimpleDateFormat> formatThreadLocal = ThreadLocal.withInitial(() -> new SimpleDateFormat());
             String format = formatThreadLocal.get().format(new Date());
             System.out.println(format);
-            
+
         }
     }
+
+    @Test
+    public void test2() {
+        LocalDateTime parse = LocalDateTime.parse("2020-05-06 23:59:59", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        System.out.println(parse);
+    }
+
+
 }
