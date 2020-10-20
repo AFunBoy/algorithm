b180999ec56a1f46f512173781f196288577a5a4
funBoy <wangfan528@126.com>
2020-10-20 22:58:45 +0800

    1

--- bigpackage-Algorithm/src/main/java/com/funboy/中级/数组/递增的三元子序列.java
+++ bigpackage-Algorithm/src/main/java/com/funboy/中级/数组/递增的三元子序列.java
@@ -5,8 +5,8 @@ package com.funboy.中级.数组;
  * <p>
  * 数学表达式如下:
  * <p>
- * 如果存在这样的 i, j, k,  且满足 0 ≤ i < j < k ≤ n-1，
- * 使得 arr[i] < arr[j] < arr[k] ，返回 true ; 否则返回 false 。
+ * 如果存在这样的  i, j, k,    且满足  0 ≤ i < j < k ≤ n-1，
+ * 使得  arr[i] < arr[j] < arr[k] ，返回 true ;  否则返回 false 。
  * 说明: 要求算法的时间复杂度为 O(n)，空间复杂度为 O(1) 。
  * <p>
  * 示例 1:
